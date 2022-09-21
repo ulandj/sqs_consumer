95de1005cfdbdc96d7782b69c25d633f605853de
Ulan Djamanbalaev <udjamanbalaev@gmail.com>
2022-09-21 06:48:38 +0100

    Delete adapter methods since it is not aim to enqueue any job

--- lib/active_job/queue_adapters/amazon_sqs_consumer_adapter.rb
+++ lib/active_job/queue_adapters/amazon_sqs_consumer_adapter.rb
@@ -5,56 +5,7 @@ require 'aws-sdk-sqs'
 module ActiveJob
   module QueueAdapters
 
-    class AmazonSqsConsumerAdapter
-
-      def enqueue(job)
-        _enqueue(job)
-      end
-
-      def enqueue_at(job, timestamp)
-        delay = (timestamp - Time.now.to_f).floor
-        raise ArgumentError, 'Unable to queue a job with a delay great than 15 minutes' if delay > 15.minutes
-        _enqueue(job, nil, delay_seconds: delay)
-      end
-
-      private
-
-      def _enqueue(job, body = nil, send_message_opts = {})
-        body ||= job.serialize
-        queue_url = Aws::Rails::SqsConsumer.config.queue_url_for(job.queue_name)
-        send_message_opts[:queue_url] = queue_url
-        send_message_opts[:message_body] = Aws::Json.dump(body)
-        send_message_opts[:message_attributes] = message_attributes(job)
-
-        if Aws::Rails::SqsConsumer.fifo?(queue_url)
-          # job_id is unique per initialization of job
-          # Remove it from message dup id to ensure run-once behavior
-          # with ActiveJob retries
-          send_message_opts[:message_deduplication_id] =
-            Digest::SHA256.hexdigest(Aws::Json.dump(body.except('job_id')))
-
-          message_group_id = job.message_group_id if job.respond_to?(:message_group_id)
-          message_group_id ||= Aws::Rails::SqsConsumer.config.message_group_id
-
-          send_message_opts[:message_group_id] = message_group_id
-        end
-
-        Aws::Rails::SqsConsumer.config.client.send_message(send_message_opts)
-      end
-
-      def message_attributes(job)
-        {
-          'sqs_consumer_class' => {
-            string_value: job.class.to_s,
-            data_type: 'String'
-          },
-          'sqs_consumer_version' => {
-            string_value: Aws::Rails::VERSION,
-            data_type: 'String'
-          }
-        }
-      end
-    end
+    class AmazonSqsConsumerAdapter; end
 
     # create an alias to allow `:amazon` to be used as the adapter name
     # `:amazon` is the convention used for ActionMailer and ActiveStorage
--- lib/active_job/queue_adapters/amazon_sqs_consumer_async_adapter.rb
+++ lib/active_job/queue_adapters/amazon_sqs_consumer_async_adapter.rb
@@ -15,27 +15,6 @@ module ActiveJob
     #
     # config.active_job.queue_adapter = :amazon_sqs_async
     class AmazonSqsConsumerAsyncAdapter < AmazonSqsConsumerAdapter
-
-      private
-
-      def _enqueue(job, body = nil, send_message_opts = {})
-        # FIFO jobs must be queued in order, so do not queue async
-        queue_url = Aws::Rails::SqsConsumer.config.queue_url_for(job.queue_name)
-        if Aws::Rails::SqsConsumer.fifo?(queue_url)
-          super(job, body, send_message_opts)
-        else
-          # Serialize is called here because the job’s locale needs to be
-          # determined in this thread and not in some other thread.
-          body = job.serialize
-          Concurrent::Promises
-            .future { super(job, body, send_message_opts) }
-            .rescue do |e|
-              Rails.logger.error "Failed to queue job #{job}. Reason: #{e}"
-              error_handler = Aws::Rails::SqsConsumer.config.async_queue_error_handler
-              error_handler&.call(e, job, send_message_opts)
-            end
-        end
-      end
     end
   end
 end
